87e3079eedf0afefa78816008b6035da6793c73d
ryan65728 <trig5566@gmail.com>
2025-01-31 23:00:28 +0800

    Create 328.cpp

---
+++ 328.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        ListNode* odd = head;
+        ListNode* even = nullptr;
+    }
+};
