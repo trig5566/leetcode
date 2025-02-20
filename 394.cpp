c148f6d54f9d6da346d1e68680fda3e014ec76a1
trig5566 <trig5566@gmail.com>
2025-02-20 18:14:14 +0800

    Update 394.cpp

--- 394.cpp
+++ 394.cpp
@@ -13,79 +13,50 @@ using namespace std;
 
 class Solution {
     public:
-#define NUM_STAT 0
-#define CHA_STAT 1
-#define BRAC_STAT 2
         string decodeString(string s) {
             int ptr = 0;
-            int state = NUM_STAT;
             string result = "";
-            stack<int> st_num;
-            stack<string> st_cha;
-            int i = 0;
-            for(i = 0; i < s.size(); i++){
-                if(s[i] == '['){
-                    if(state == CHA_STAT){
-                        st_cha.push(s.substr(ptr,i-ptr));
-                    } else if (state == NUM_STAT){
-                        st_num.push(stoi(s.substr(ptr,i-ptr)));
+            stack<pair<string,int>> deal;
+            string tmp_s = "";
+            int tmp_i = 1;
+            int stat = 0;
+            for(int i = 0; i < s.size(); i++){
+                //cout<<tmp_s<<endl;
+                if(s[i] == ']'){
+                    auto tmp = deal.top();
+                    deal.pop();
+                    string repeat = "";
+                    for(int j = 0;j < tmp.second;j++){
+                        repeat += tmp_s;
                     }
-                    state = BRAC_STAT;
-                } else if (s[i] == ']'){
-                    if(state = CHA_STAT)
-                        st_cha.push(s.substr(ptr,i-ptr));
-                    int num = st_num.top();
-                    string cha = st_cha.top();
-                    string tmp = "";
-                    st_num.pop();
-                    st_cha.pop();
-                    for(int j = 0;j < num;j++){
-                        tmp += cha;
-                    }
-                    if(!st_num.empty()){
-                        if(!st_cha.empty()){
-                            cha = st_cha.top();
-                            st_cha.pop();
-                        }else
-                            cha = "";
-                        cha += tmp;
-                        st_cha.push(cha);
-                    } else 
-                        result += tmp;
-                    state = BRAC_STAT;
-                } else if(s[i] >= '0' && s[i] <= '9'){
-                    if(state == CHA_STAT){
-                        st_cha.push(s.substr(ptr,i-ptr));
-                        state = NUM_STAT;
-                        ptr = i;
-                    } else if(state == BRAC_STAT){
-                        state = NUM_STAT;
-                        ptr = i;
-                    }
+                    tmp_s = tmp.first + repeat;
                 } else if(s[i] >= 'a' && s[i] <= 'z'){
-                    if(state == NUM_STAT){
-                        st_num.push(stoi(s.substr(ptr,i-ptr)));
-                        state = CHA_STAT;
-                        ptr = i; 
-                    } else if(state == BRAC_STAT){
-                        state = CHA_STAT;
+                    if(stat == 0){
+                        stat = 1;
+                    }
+                    tmp_s += s[i];
+                } else if(s[i] >= '0' && s[i] <= '9'){
+                    if(stat == 0 || stat == 1){
                         ptr = i;
-                    } 
+                        stat = 2;
+                    }
+                } else if(s[i] == '['){
+                    tmp_i = stoi(s.substr(ptr,i - ptr));
+                    deal.push({tmp_s,tmp_i});
+                    tmp_s = "";
+                    tmp_i = 1;
+                    stat = 0;
                 }
-                    
+            
             }
-            if(state == CHA_STAT)
-                result += s.substr(ptr,i-ptr);
-            if(!st_cha.empty())
-                result = st_cha.top() + result;
-            return result;
+            return tmp_s;
         }
 };
 
 int main()
 {
     Solution a;
-   // string test = "asdc2[12[3[a]]]asd1[d]fc10[a]rfd";
+    //string test = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
     string test = "3[a2[c]]";
     cout<<a.decodeString(test);
     return 0;
