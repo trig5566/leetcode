76d45397ddbda1d0810b621c7d52aab3f2269b49
ryan65728 <trig5566@gmail.com>
2025-03-09 14:47:28 +0800

    Update 450.cpp

--- 450.cpp
+++ 450.cpp
@@ -29,6 +29,45 @@ struct TreeNode {
 class Solution {
     public:
         TreeNode* deleteNode(TreeNode* root, int key) {
-            
+            if(root == NULL)
+                return root;
+            TreeNode* it = root;
+            TreeNode* pre = NULL;
+            while(it != NULL){
+                if(key == it->val){
+                    if(it->left != NULL){
+                        it
+                    }
+                    break;
+                } else if(key > it->val)
+                    it = it->right;
+                else
+                    it = it->left;
+                pre = it;
+            }
+        }
+
+        void move_to_end(TreeNode* tar, TreeNode* pre){
+            if(tar->left == NULL && tar->right == NULL){
+                tar = NULL;
+            }else if(tar->left != NULL){
+                TreeNode* swap_tar = (tar->left->left == NULL?tar->left:tar->left->left);
+                if(pre->left == tar)
+                    pre->left = 
+            }
+        }
+
+        TreeNode* swap_tn(TreeNode* a, TreeNode* b){
+            TreeNode* tmp_r = a->right == b?a:a->right;
+            TreeNode* tmp_l = a->left == b?a:a->left;
+            TreeNode* parent;
+            if(a->right == b || a->left == b){
+                parent = b;
+            }
+            a->left = b->left;
+            a->right = b->right;
+            b->left = tmp_l;
+            b->right = tmp_r;
+            return b;
         }
 };
