03cf4d154d45909fe679d71e59a78288dfe155b2
ryan65728 <trig5566@gmail.com>
2025-03-25 06:39:45 +0800

    Update 503.cpp

--- 503.cpp
+++ 503.cpp
@@ -20,6 +20,7 @@ class Solution {
         vector<int> nextGreaterElements(vector<int>& nums) {
             int n = nums.size();
             stack<int> st;
+            vector<bool> record(n,0);
             vector<int> result(n,-1);
             if(n <= 1)
                 return result;
@@ -28,13 +29,39 @@ class Solution {
             while(st.size() > 0){
                 if(st.top() == i)
                     break;
-                if(nums[st.top()] >= nums[i] && result[i] == -1){
-                    st.push(i);
+                if(nums[st.top()] >= nums[i]){
+                    if(!record[i]){
+                        st.push(i);
+                        record[i] = 1;
+                    }
+                } else {
+                    while(st.size() > 0 && nums[st.top()] < nums[i]){
+                        result[st.top()] = nums[i];
+                        st.pop();
+                    }
+                    if(!record[i]){
+                        st.push(i);
+                        record[i] = 1;
+                    }
                 }
 
                 i++;
                 if(i >= n)
                     i = 0;
             }
+            return result;
         }
+        
     };
+
+
+int main()
+{
+    Solution a;
+    vector<int> test = {2,2,1,1,3,2};
+    vector<int> result = a.nextGreaterElements(test);
+    for(int i:result){
+        cout<<i<<",";
+    }
+    return 0;
+}
