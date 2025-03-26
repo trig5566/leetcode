0ea7b48bc06665cd05ad0a8227a98a376117f981
trig5566 <trig5566@gmail.com>
2025-03-26 16:41:33 +0800

    Create 513.cpp

---
+++ 513.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <cmath>
+#include <random>
+#include <bitset>
+
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+ class Solution {
+    public:
+        int findBottomLeftValue(TreeNode* root) {
+            
+        }
+};
