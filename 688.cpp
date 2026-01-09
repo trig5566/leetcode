f636184f879d5953bacb50ab3e0876bb51243d59
ryan65728 <trig5566@gmail.com>
2026-01-09 08:15:46 +0800

    Update 688.cpp

--- 688.cpp
+++ 688.cpp
@@ -13,19 +13,42 @@
 #include <random>
 #include <bitset>
 
-using namespace std;
-
-#define CHECK_ON_BOARD(x, y, move_x, move_y, n)  if(x+move_x >= 0 && x+move_x < n && y+move_y >= 0 && y+move_y < n)  
+using namespace std; 
 
 class Solution {
 public:
-    vector<vector<double>> chessBoard;
+    vector<vector<vector<double>>> chessBoard;
     double knightProbability(int n, int k, int row, int column) {
-        chessBoard = vector<vector<double>>(n, vector<double>(n, 0));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-
+        double result = 0;
+        chessBoard = vector<vector<vector<double>>>(k+1, vector<vector<double>>(n, vector<double>(n, 0)));
+        chessBoard[0][row][column] = 1;
+        for(int l = 1; l <= k; l++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(i - 2 >= 0 && j - 1 >= 0)
+                        chessBoard[l][i][j] += chessBoard[l-1][i-2][j-1]*0.125;
+                    if(i - 2 >= 0 && j + 1 < n)
+                        chessBoard[l][i][j] += chessBoard[l-1][i-2][j+1]*0.125;
+                    if(i + 2 < n && j - 1 >= 0)
+                        chessBoard[l][i][j] += chessBoard[l-1][i+2][j-1]*0.125;
+                    if(i + 2 < n && j + 1 < n)
+                        chessBoard[l][i][j] += chessBoard[l-1][i+2][j+1]*0.125;
+                    if(i - 1 >= 0 && j - 2 >= 0)
+                        chessBoard[l][i][j] += chessBoard[l-1][i-1][j-2]*0.125;
+                    if(i - 1 >= 0 && j + 2 < n)
+                        chessBoard[l][i][j] += chessBoard[l-1][i-1][j+2]*0.125;
+                    if(i + 1 < n && j - 2 >= 0)
+                        chessBoard[l][i][j] += chessBoard[l-1][i+1][j-2]*0.125;
+                    if(i + 1 < n && j + 2 < n)
+                        chessBoard[l][i][j] += chessBoard[l-1][i+1][j+2]*0.125;
+                }
             }
         }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                result += chessBoard[k][i][j];
+            }
+        }
+        return result;
     }
 };
